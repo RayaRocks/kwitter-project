cc9e7e34fae7255bbc7ee39fd05e472d90f15577
RayaRocks <107084584+RayaRocks@users.noreply.github.com>
2022-11-07 18:52:29 -0500

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,20 @@
+function addUser(){
+
+    user_name = document.getElementById("user_name").value;
+
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "kwitter_room.html";
+}
+function addRoom()
+{
+    room_name = document.getElementById("room_name").value;
+
+    firebase.database().ref("/").child(room_name).update({
+        purpose : "adding room name"
+    });
+
+    localStorage.setItem("room_name", room__name);
+
+    window.location = "kwitter_page.html";
+}
